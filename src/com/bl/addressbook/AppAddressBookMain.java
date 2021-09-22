a0f6eb3f6cf1f52dbd0207db493bdfa71a0a1f91
RohitGupta8 <therohitg@gmail.com>
2021-09-22 15:53:49 +0530

    ADD : Ability To Edit Saved Contact

--- src/com/bl/addressbook/AppAddressBookMain.java
+++ src/com/bl/addressbook/AppAddressBookMain.java
@@ -1,6 +1,7 @@
 package com.bl.addressbook;
 
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class AppAddressBookMain {
@@ -12,14 +13,50 @@ public class AppAddressBookMain {
         int ch = 0;
         while (ch == 0) {
             Scanner scanner = new Scanner(System.in);
-            System.out.print("1. = Add Contacts\n2. = Display Contact\n3. = Exit\nEnter Your Choice: ");
+            System.out.print("1. = Add Contacts\n2. = Display Contact\n3. = Edit Contact\n4. = Exit\nEnter Your Choice: ");
             int choice = scanner.nextInt();
             switch (choice) {
                 case 1 -> appAddressBookMain.createContact();
                 case 2 -> appAddressBookMain.display();
-                case 3 -> ch = 1;
+                case 3 -> appAddressBookMain.edit();
+                case 4 -> ch = 1;
                 default -> System.out.println("Plz Enter 1 or 2 or 3  only");
             }
+        }
+    }
+    private void edit() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Name To Edit from Contact list: ");
+        String firstNam = scanner.nextLine();
+        for (Contact value : contactList) {
+            if (Objects.equals(firstNam, value.getFirstName())) {
+                System.out.print("Enter First Name: ");
+                String firstName = scanner.nextLine();
+                value.setFirstName(firstName);
+                System.out.print("Enter Last Name: ");
+                String lastName = scanner.nextLine();
+                value.setLastName(lastName);
+                System.out.print("Enter Address: ");
+                String address = scanner.nextLine();
+                value.setAddress(address);
+                System.out.print("Enter City: ");
+                String city = scanner.nextLine();
+                value.setCity(city);
+                System.out.print("Enter State: ");
+                String state = scanner.nextLine();
+                value.setState(state);
+                System.out.print("Enter ZipCode: ");
+                String zipcode = scanner.nextLine();
+                value.setZipCode(zipcode);
+                System.out.print("Enter Phone Number: ");
+                String number = scanner.nextLine();
+                value.setNumber(number);
+                System.out.print("Enter Email id: ");
+                String email = scanner.nextLine();
+                value.setEmail(email);
+                System.out.println("\n Success !  contact updated.....\n");
+
+            } else System.out.println("The name entered is incorrect.");
         }
     }
 
