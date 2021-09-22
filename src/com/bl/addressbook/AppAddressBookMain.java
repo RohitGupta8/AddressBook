17024cdf50da522ca64ed4cf372e7613be100cc7
RohitGupta8 <therohitg@gmail.com>
2021-09-22 14:24:05 +0530

    Add : Welcome Message

---
+++ src/com/bl/addressbook/AppAddressBookMain.java
@@ -0,0 +1,9 @@
+package com.bl.addressbook;
+
+public class AppAddressBookMain {
+
+    public static void main(String[] args) {
+        System.out.println("***********************************************\n          Welcome To Address Book\n***********************************************\n");
+        
+    }
+}
